2520a6e1e3c25e8d5f7129a6894208be4ee82ca6
Raúl González <gonzalv@ciencias.unam.mx>
2020-09-04 17:14:22 -0500

    [ADDED] Soluciones a clase14

--- clase14/soluciones/ej01.py
+++ clase14/soluciones/ej01.py
@@ -3,8 +3,6 @@ import os
 def sed(old, new, origin, destination):
     try:
         content = open(origin).read()
-        if not os.path.exists(destination):
-            os.popen('touch ' + destination)
         fout = open(destination, 'w')
         fout.write(content.replace(old, new))
         fout.close()
--- clase14/soluciones/ej03.py
+++ clase14/soluciones/ej03.py
@@ -1,29 +1,37 @@
 import os
 
+def format_quotes(s):
+    if '\'' in s:
+        return '"%s"' % s
+    else:
+        return "'%s'" % s
+
 def get_hash(file_path):
-    cmd = "md5sum '%s'" % file_path
+    cmd = "md5sum %s" % format_quotes(file_path)
     fp = os.popen(cmd)
-    hash = fp.read()[0]
+    hash = fp.read().split()[0]
     fp.close()
     return hash
 
 def has_extension(file_name, extension):
     return file_name.split(".")[-1] == extension
 
-def search_duplicates(root="./", extension="mp3", files=dict(), duplicates=dict()):
+def search_duplicates(root="./", extension="mp3", files=dict()):
     path, dirnames, filenames = os.walk(root).__next__()
     path = os.path.abspath(path)
     for file in filenames:
         file_path = os.path.join(path, file)
         if has_extension(file_path, extension):
-            hash = get_hash(file_path)
-            paths = d.setdefault(hash, [])
-            paths.append(file_path)
-            if len(paths) > 1:
-                duplicates[hash] = paths
+            files.setdefault(get_hash(file_path), []).append(file_path)
     for dir in dirnames:
         dir_path = os.path.join(path, dir)
-        search_duplicates(dir_path, extension, files, duplicates)
-    return duplicates
+        search_duplicates(dir_path, extension, files)
+    return files
 
-search_duplicates("/home/raul-ga/Música")
+def print_duplicates(d):
+    for hash, paths in d.items():
+        if len(paths) > 1:
+            print(paths)
+
+d = search_duplicates("/home/raul-ga/Música")
+print_duplicates(d)
